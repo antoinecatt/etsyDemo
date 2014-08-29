0fe9ac2c1633a2689e32fada8fac96bd77a92ebc
antoinecatt <antoinecatt@gmail.com>
2014-08-29 15:56:50 -0700

    fixed listings model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,11 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
-   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",    
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :path => ":style/:id_:filename"
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
   end
 
   validates :name, :description, :price, presence: true
