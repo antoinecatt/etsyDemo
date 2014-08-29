8c75372153169879a98b9039b7f1d53a4d728718
antoinecatt <antoinecatt@gmail.com>
2014-08-29 14:19:22 -0700

    removed bug listings model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,6 @@ class Listing < ActiveRecord::Base
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     :path => ":style/:id_:filename"
   end
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
